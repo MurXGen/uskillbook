75e225b0a7015bf6b1de0b7b64b25be16aa80df7
Murthy Thevar <152300792+MurXGen@users.noreply.github.com>
2025-02-10 09:55:47 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,8 @@
-dotenv.config();
+
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
+dotenv.config();
 const mongoose = require("mongoose");
 const orderRoutes = require("./routes/orderRoutes");
 
