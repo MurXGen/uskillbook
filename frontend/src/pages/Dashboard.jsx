a1c3d43608dff58612f75b5f6b2c1e6232672bf0
Murthy Thevar <152300792+MurXGen@users.noreply.github.com>
2025-03-03 01:45:43 +0530

    Dashboard UI

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -3,6 +3,11 @@ import { Link } from "react-router-dom";
 import Navbar from '../components/Navbar'
 import Bag from '../assets/Bag.png'
 import '../index.css';
+import AddOrder from '../assets/addOrder.svg'
+import CashFlow from '../assets/cashFlow.svg'
+import Supplier from '../assets/supplier.svg'
+import ReqBook from '../assets/ReqBook.svg'
+import History from '../assets/History.svg'
 
 function Dashboard() {
   const navigate = useNavigate();
@@ -10,33 +15,77 @@ function Dashboard() {
   return (
     <>
       <div className="dashboard">
-        {/* Pay Now start */}
-        <div className="payNow">
-          <div className="title">
-            <span className="desc">Powered by <strong>Razorpay</strong></span>
-            <span className="title">Make Seamless Payment with us</span>
-          </div>
-          <div className="features">
-            <div className="feature">
-              <span class="material-symbols-outlined">
-                check
-              </span>
-              <span>Track your payments</span>
+        <Link to='/checkout'>
+          <div className="addOrder">
+            <div className="illustration">
+              <img src={AddOrder} alt="" />
             </div>
-            <div className="feature">
-              <span class="material-symbols-outlined">
-                check
-              </span>
-              <span>Earn upto 10% worth cashback on every purchase</span>
+            <div className="content">
+            <span>Add Order</span>
+            <span class="material-symbols-outlined">
+              arrow_right
+            </span>
             </div>
           </div>
-          <Link className="proceed" to='/checkout'>Proceed to Pay<span class="material-symbols-outlined">
-            arrow_right
-          </span></Link>
+        </Link>
+        <div className="otherDirects">
+        <Link to='/cashflow'>
+          <div className="otherBox">
+            <div className="illustration">
+              <img src={CashFlow} alt="" />
+            </div>
+            <div className="otherBoxContent">
+            <span>Cashflow</span>
+            <span class="material-symbols-outlined">
+              arrow_right
+            </span>
+            </div>
+          </div>
+        </Link>
+
+        <Link to='/dealers'>
+          <div className="otherBox">
+            <div className="illustration">
+              <img src={Supplier} alt="" />
+            </div>
+            <div className="otherBoxContent">
+            <span>Supplier</span>
+            <span class="material-symbols-outlined">
+              arrow_right
+            </span>
+            </div>
+          </div>
+        </Link>
+
+        <Link to='/transaction-history'>
+          <div className="otherBox">
+            <div className="illustration">
+              <img src={History} alt="" />
+            </div>
+            <div className="otherBoxContent">
+            <span>History</span>
+            <span class="material-symbols-outlined">
+              arrow_right
+            </span>
+            </div>
+          </div>
+        </Link>
+
+        <Link to='/need-orders'>
+          <div className="otherBox">
+            <div className="illustration">
+              <img src={ReqBook} alt="" />
+            </div>
+            <div className="otherBoxContent">
+            <span>Request a Book</span>
+            <span class="material-symbols-outlined">
+              arrow_right
+            </span>
+            </div>
+          </div>
+        </Link>
+      
         </div>
-
-        {/* Pay Now end */}
-
       </div>
     </>
   );
